d67296a7d2f5e15589db1ab11a68b8cee6be2905
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-03-14 04:41:14 +0100

    Fix tools/msvc_setup.bat

--- tools/msvc_setup.bat
+++ tools/msvc_setup.bat
@@ -1,3 +1,6 @@
-$installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -property installationpath
-Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
-Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation -DevCmdArguments '-arch=x64 -no_logo'
+@echo off
+REM Find Visual Studio installation path using vswhere.exe
+FOR /F "delims=" %%i IN ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -property installationpath') DO SET InstallPath=%%i
+
+REM Activate Visual Studio Developer Command Prompt environment
+CALL "%InstallPath%\Common7\Tools\VsDevCmd.bat" -arch=x64 -no_logo
