ca91059de5a6f01f4b1ea628c3aadc62fbb45e2b
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-03-17 04:16:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,30 +20,16 @@ from ccimport import compat
 import subprocess 
 import re 
 
-# Package meta-data.
 NAME = 'spconv'
 RELEASE_NAME = NAME
 deps = ["cumm"]
 cuda_ver = os.environ.get("CUMM_CUDA_VERSION", "")
-
-# is_ci_build = cuda_ver != ""
-# if not cuda_ver:
-#     nvcc_version = subprocess.check_output(["nvcc", "--version"
-#                                             ]).decode("utf-8").strip()
-#     nvcc_version_str = nvcc_version.split("\n")[3]
-#     version_str: str = re.findall(r"release (\d+.\d+)",
-#                                     nvcc_version_str)[0]
-#     cuda_ver = version_str
-
 if cuda_ver:
-    cuda_ver_str = cuda_ver.replace(".", "") # 10.2 to 102
-
+    cuda_ver_str = cuda_ver.replace(".", "")
     RELEASE_NAME += "-cu{}".format(cuda_ver_str)
     deps = ["cumm-cu{}>=0.8.4, <1.0.0".format(cuda_ver_str)]
 else:
     deps = ["cumm>=0.8.4, <1.0.0"]
-
-
 
 DESCRIPTION = 'spatial sparse convolution'
 URL = 'https://github.com/traveller59/spconv'
@@ -52,31 +38,21 @@ AUTHOR = 'Yan Yan'
 REQUIRES_PYTHON = '>=3.9'
 VERSION = None
 
-# What packages are required for this module to be executed?
 REQUIRED = ["pccm>=0.4.16", "ccimport>=0.4.4", "pybind11>=2.6.0", "fire", "numpy", *deps]
 
-# What packages are optional?
 EXTRAS = {
     # 'fancy feature': ['django'],
 }
 
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(str(Path(__file__).parent))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
     with open('version.txt', 'r') as f:
@@ -84,7 +60,6 @@ if not VERSION:
 else:
     version = VERSION
 cwd = os.path.dirname(os.path.abspath(__file__))
-
 
 def _convert_build_number(build_number):
     parts = build_number.split(".")
@@ -94,7 +69,6 @@ def _convert_build_number(build_number):
         return build_number
     else:
         raise NotImplementedError
-
 
 env_suffix = os.environ.get("SPCONV_VERSION_SUFFIX", "")
 if env_suffix != "":
@@ -107,7 +81,6 @@ with open(version_path, 'w') as f:
 
 class UploadCommand(Command):
     """Support setup.py upload."""
-
     description = 'Build and publish the package.'
     user_options = []
 
@@ -115,37 +88,27 @@ class UploadCommand(Command):
     def status(s):
         """Prints things in bold."""
         print('\033[1m{0}\033[0m'.format(s))
-
     def initialize_options(self):
         pass
-
     def finalize_options(self):
         pass
-
     def run(self):
         try:
             self.status('Removing previous builds...')
             rmtree(os.path.join(here, 'dist'))
         except OSError:
             pass
-
         self.status('Building Source and Wheel (universal) distribution...')
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(
             sys.executable))
-
         self.status('Uploading the package to PyPI via Twine...')
         os.system('twine upload dist/*')
-
         self.status('Pushing git tags...')
         os.system('git tag v{0}'.format(about['__version__']))
         os.system('git push --tags')
-
         sys.exit()
 
-
-
 disable_jit = os.getenv("SPCONV_DISABLE_JIT", None)
-
 if disable_jit is not None and disable_jit == "1":
     cmdclass = {
         'upload': UploadCommand,
@@ -173,7 +136,6 @@ if disable_jit is not None and disable_jit == "1":
     cu = GemmMainUnitTest(all_shuffle)
     convcu = ConvMainUnitTest(all_imp)
     convcu.namespace = "cumm.conv.main"
-
     cu.namespace = "cumm.gemm.main"
     std = "c++17"
     if cuda_ver:
@@ -217,12 +179,9 @@ if disable_jit is not None and disable_jit == "1":
                       verbose=True)
     ]
 else:
-    cmdclass = {
-        'upload': UploadCommand,
-    }
+    cmdclass = {'upload': UploadCommand,}
     ext_modules = []
 
-# Where the magic happens:
 setup(
     name=RELEASE_NAME,
     version=about['__version__'],
@@ -234,8 +193,6 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=('tests', )),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
     entry_points={
         'console_scripts': [],
     },
@@ -244,13 +201,8 @@ setup(
     include_package_data=True,
     license='Apache License 2.0',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
